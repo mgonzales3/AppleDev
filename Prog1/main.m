d7ae0ca13fb86fe92f93c2cc63d5022f723ef98f
manny gonzales <mgonzales3@yahoo.com>
2013-12-13 21:16:50 -0600

    Initial Commit

---
+++ Prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Prog1
+//
+//  Created by manuel gonzales iii on 12/13/13.
+//  Copyright (c) 2013 manuel gonzales iii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
